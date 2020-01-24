1c874d64eb333691e6c8f49fb3eb77466bc498ad
Igor-Belokon <igorbelokon8@gmail.com>
2020-01-24 09:55:39 +0200

    +app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Users from "../src/components/users/users";
 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store";
-// const {store , persistor} =createStore()
+import createStore from "./store";
+const { store, persistor } = createStore();
 
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <Router>
-          <>
-            <Route path="/users" component={Users} />
-            <Route path="/" component={Courses} />
-          </>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <>
+              <Route path="/users" component={Users} />
+              <Route path="/" component={Courses} />
+            </>
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
